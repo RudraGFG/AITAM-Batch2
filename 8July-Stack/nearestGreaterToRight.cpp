466dcf1b5df2fbb51422842fdc7ab04394d6ee9e
RudraGFG <rudra.makhija@geeksforgeeks.org>
2025-07-08 16:01:07 +0530

    Create nearestGreaterToRight.cpp

---
+++ 8July-Stack/nearestGreaterToRight.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {100, 200, 300, 400, 4};
+    
+    int n = arr.size();
+    
+    stack<int> st;
+    vector<int> ans(n, 0);
+    
+    for(int i=n-1; i>=0; i--){
+        // pop elements from stack
+        while(!st.empty() && st.top() <= arr[i]) st.pop();
+        
+        if(st.empty()) ans[i] = -1;
+        else ans[i] = st.top();
+        
+        st.push(arr[i]);
+    }
+    
+    for(int i : ans) cout << i << " ";
+
+    return 0;
+}
