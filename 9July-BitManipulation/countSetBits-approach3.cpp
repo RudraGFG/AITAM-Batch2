cbc06d5c3ea49642856a2e5d97f69c43e8d22477
RudraGFG <rudra.makhija@geeksforgeeks.org>
2025-07-10 19:28:08 +0530

    Create countSetBits-approach3.cpp

---
+++ 9July-BitManipulation/countSetBits-approach3.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int n = 7;
+	int cnt = 0; // to track the no. of set bits in 'n'
+
+	while (n > 0) { // #times loop runs = logn
+
+		// 1. extract the rightmost bit of n
+
+		int rightmostbit = n & 1;
+
+		if (rightmostbit == 1) {
+			cnt++;
+		}
+
+		n = n >> 1;
+
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
