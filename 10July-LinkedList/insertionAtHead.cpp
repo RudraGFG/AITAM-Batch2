f0a6edeb58721c9b66fed3820132398c80a5f30a
RudraGFG <rudra.makhija@geeksforgeeks.org>
2025-07-10 18:47:36 +0530

    Create insertionAtHead.cpp

---
+++ 10July-LinkedList/insertionAtHead.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+
+public:
+
+	int val;
+	ListNode* next;
+
+	ListNode(int val) {
+		this->val = val;
+		this->next = NULL;
+	}
+
+};
+
+void insertAtHead(ListNode*& head, int val) {
+    // passing address of the head-node by reference
+
+	ListNode* nn = new ListNode(val);
+	nn->next = head;
+	head = nn;
+}
+
+
+void printLL(ListNode* head){
+    ListNode* i = head;
+    int cnt = 0;
+    
+    while(i != NULL){
+        cout << i->val << "->";
+        i = i->next;
+        
+        cnt++;
+    }
+    
+    cout << endl;
+    cout << cnt;
+}
+
+int main() {
+
+	ListNode* head = NULL; //empty linked list
+
+	insertAtHead(head, 50);
+	insertAtHead(head, 40);
+	insertAtHead(head, 30);
+	insertAtHead(head, 20);
+	insertAtHead(head, 10);
+
+
+	printLL(head);
+
+	return 0;
+}
